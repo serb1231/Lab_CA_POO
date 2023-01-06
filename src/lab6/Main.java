12eb6bc5c1c7709b9f2073d61b21b89fb6cb418f
serb1231 <serban.ionescu15@gmail.com>
2023-01-06 19:42:31 +0200

    Initialize lab6

---
+++ src/lab6/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static Random rand = new Random(20);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int taskNum = scanner.nextInt();
+
+        switch(taskNum) {
+            case 1:
+                // TODO: uncomment for Task1
+                // Car mercedes = new Car(20000, Car.CarType.MERCEDES, 2019);
+                // Car fiat = new Car(7000, Car.CarType.FIAT, 2020);
+                // Car skoda = new Car(12000, Car.CarType.SKODA, 2022);
+                // Dealership dealership = new Dealership();
+                break;
+            case 2:
+                // TODO: uncomment for Task2
+                // dealership = new Dealership();
+                break;
+            case 3:
+                // TODO: uncomment for Task3
+                // dealership = new Dealership();
+                // TODO: write solution for Task3 here
+                break;
+            case 4:
+                // TODO: write solution for Task4 here
+                break;
+        }
+    }
+}
